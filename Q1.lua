0678d13e1e4d445fbc05ac90f91e6cff7f971335
BrendonHorning <43552900+BrendonHorning@users.noreply.github.com>
2024-04-22 12:58:13 -0700

    Add files via upload
    fixed errors in Q1 and Q2.

--- Q1.lua
+++ Q1.lua
@@ -4,9 +4,9 @@ local function releaseStorage(player)
 	player:setStorageValue(outStorage, -1)
 end
 
--- function onLogout(player)
+ function onLogout(player)
 	if player:getStorageValue(outStorage) == 1 then
-		releaseStorage(player) -- delay necessary?
+		addEvent(releaseStorage, 1000, player) -- assuming delay is necessary for logout to finalize?
 	end
 	return true
-end 
+end
--- Q2.lua
+++ Q2.lua
@@ -1,5 +1,6 @@
 function printSmallGuildNames(memberCount)
-	local selectGuildQuery = "SELECT name FROM guilds WHERE guild_id IN (SELECT guild_id FROM guild_membership GROUP BY guild_id HAVING COUNT(guild_id) < %d);" -- no max_members in database provided; if assumed exists, ignore channge
+	local selectGuildQuery = "SELECT name FROM guilds WHERE guild_id IN (SELECT guild_id FROM guild_membership GROUP BY guild_id HAVING COUNT(guild_id) < %d);"
+	-- no max_members in database provided; if assumed exists, ignore above change
 	local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount)) -- assuming db called elsewhere, otherwise needs db = require "db.mysql"?
 	local guildName = resultId.getString("name") -- fixed variable name, result -> resultID
 	print(guildName)
