f7c17f8cd5363e97cf75344c5191e783861ef976
BrendonHorning <43552900+BrendonHorning@users.noreply.github.com>
2024-04-22 18:01:35 -0700

    reupload of Q5 modified files
    streamlined code for Q5 to be more scalable and added further comments. video updated to reflect changes made to create closer accuracy to example.

--- Q5.lua
+++ Q5.lua
@@ -35,52 +35,42 @@ local array4 = {
 	{0, 0, 0, 1, 0, 0, 0}
 }
 
-local combat1 = Combat() -- setting combat settings for each. can definitely be streamlined
-combat1:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat1:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat1:setArea(createCombatArea(array1))
+local arrays = {array1, array2, array3, array4} -- put the spell arrays into an array in order to get count for variables.
+local numAreas = #arrays -- used in iteration through all areas for scalability
+local castDelay = 250 -- determines delay between each area wave during the spell's cast
+local loops = 3 -- number of loops through spell cast
 
+local combat1 = Combat() -- creating combat vars 
 local combat2 = Combat()
-combat2:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat2:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat2:setArea(createCombatArea(array2))
-
 local combat3 = Combat()
-combat3:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat3:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat3:setArea(createCombatArea(array3))
-
 local combat4 = Combat()
-combat4:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat4:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat4:setArea(createCombatArea(array4))
 
-local function cast1(creature, variant) -- cast functions for addEvent. can definitely be streamlined
-    combat1:execute(creature, variant)
-    end
+local combats = {combat1, combat2, combat3, combat4} -- sets common parameters across all combats + area based on array position
+for i, combat in ipairs(combats) do
+	combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+	combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+	combat:setArea(createCombatArea(arrays[i]))
+end
 
-local function cast2(creature, variant)
-    combat2:execute(creature, variant)
-    end
-
-local function cast3(creature, variant)
-    combat3:execute(creature, variant)
-    end
-        
-local function cast4(creature, variant)
-    combat4:execute(creature, variant)
-    end
+local function cast(creature, variant, index) -- check index for which area to execute. could probably use index with the combats array in some way but it was returning errors.
+	if index == 1 then
+		combat1:execute(creature, variant)
+	elseif index == 2 then
+		combat2:execute(creature, variant)
+	elseif index == 3 then
+		combat3:execute(creature, variant)
+	elseif index == 4 then
+		combat4:execute(creature, variant)
+	end
+end
 
 -- no damage calcs just for proof of concept
 
-function onCastSpell(creature, variant) -- addEvent rotations. can definitely be streamlined
-    addEvent(cast2, 250, creature.uid, variant)
-    addEvent(cast3, 500, creature.uid, variant)
-    addEvent(cast4, 750, creature.uid, variant)
-    addEvent(cast1, 1000, creature.uid, variant)
-    addEvent(cast2, 1250, creature.uid, variant)
-    addEvent(cast3, 1500, creature.uid, variant)
-    addEvent(cast4, 1750, creature.uid, variant)
-    addEvent(cast1, 2000, creature.uid, variant)
-    return combat1:execute(creature, variant)
-    end
+function onCastSpell(creature, variant)
+	for h=1,loops do -- spell loops through all areas based on loop variable
+		for i=1,numAreas do -- spell loops through every area
+    		addEvent(cast, i*castDelay+(castDelay*numAreas*(h-1)), creature.uid, variant, i) -- calculates delay for each iteration and wave of the spell.
+    	end
+	end
+	return combat4:execute(creature, variant) -- instant cast is returned
+end
